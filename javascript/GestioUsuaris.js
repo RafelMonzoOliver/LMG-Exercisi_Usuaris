5aa0dc8229d8ffb6a3788813b1f32f7a15bf07d7
Rafel Monzo Oliver <rafelmonzo@paucasesnovescifp.cat>
2025-03-27 13:38:09 +0100

    he continuat amb la part del formulari

--- javascript/GestioUsuaris.js
+++ javascript/GestioUsuaris.js
@@ -1,11 +1,11 @@
-import { Usuari } from "./Usuari";
-
+import { Usuari } from "./Usuari.js";
 export const llistaUsuaris = [];
 
 export class GestioUsuaris{
-    afegirUsuari(){
-  
+    afegirUsuari(Usuari){
         llistaUsuaris.push(Usuari);
+        console.log('Usuari afegit,',Usuari);
+        this.mostrarLlista();
     }
     eliminarUsuari(Usuari){
         llistaUsuaris.splice(Usuari);
--- javascript/Main.js
+++ javascript/Main.js
@@ -1,6 +1,27 @@
-import { Usuari } from "./Usuari";
-import { llistaUsuaris } from "./GestioUsuaris";
-import { GestioUsuaris } from "./GestioUsuaris";
+import { GestioUsuaris } from "./GestioUsuaris.js";
+import { Usuari } from "./Usuari.js";
+import { llistaUsuaris } from "./GestioUsuaris.js";
+
+document.addEventListener("DOMContentLoaded",function(){
+    const form = document.getElementById("formulari");
+    const button = document.getElementById("Llista");
+    const div = document.getElementById("divllista");
+    const resultat = document.getElementById("resultat");
+    const gestio = new GestioUsuaris();
+
+
+    form.addEventListener("submit",function (event){
+        event.preventDefault();
+
+        const nom = document.getElementById("nom").value;
+        const mail = document.getElementById("mail").value;
+        let usuari = new Usuari(nom,mail);
+        gestio.afegirUsuari(usuari);
+         
+
+    })
+});
+
 
 const Juan = new Usuari("Juan","Juan@Gmail.com");
 const Veronica = new Usuari("Veronica","Veronica@Gmail.com");
@@ -10,3 +31,4 @@ llistaUsuaris.push(Juan);
 llistaUsuaris.push(Veronica);
 llistaUsuaris.push(Enrique);
 console.log(llistaUsuaris.length);
+console.log(llistaUsuaris)
